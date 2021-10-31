c0b3ae005b7681b5be05b1e8464235d3203b0357
benricok <62467535+benricok@users.noreply.github.com>
2021-10-31 20:44:59 +0200

    update

---
+++ Chapter 10 - Optimizers/Optimizer_SGD-class.py
@@ -0,0 +1,16 @@
+class Optimizer_SGD:
+    # Initialize optimizer - set settings,
+    # learning rate of 1. is default fot this optimizer
+    def __init__(self, learning_rate=1.0):
+        self.learning_rate = learning_rate
+
+    # Update parameters
+    def update_params(self, layer):
+        layer.weights += -self.learning_rate * layer.dweights
+        layer.biases += -self.learning_rate * layer.dbiases
+
+
+optimizer = Optimizer_SGD()
+
+# optimizer.update_params(dense1)
+# optimizer.update_params(dense2)
