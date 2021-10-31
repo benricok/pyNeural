421ac15a4cdac0a0a3049e3e6d9b236f4c3feb49
benricok <62467535+benricok@users.noreply.github.com>
2021-10-31 16:36:09 +0200

    update

--- Chapter 7 - Derivatives/multi_numerical_derivative.py
+++ Chapter 7 - Derivatives/multi_numerical_derivative.py
@@ -2,23 +2,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+# def f(x):
+#    return 2*x**2
+
+
 def f(x):
-    return 2*x**2
+    return 5*x**3 + 2*x**2 - 5*x + 7
 
 
-x = np.arange(0, 5, 0.001)
+x = np.arange(-10, 10, 0.001)
 y = f(x)
 
 plt.plot(x, y)
 
-colors = ['k', 'g', 'r', 'b', 'c']
+colors = ['k', 'g', 'r', 'b', 'c', 'k', 'g', 'r', 'b', 'c']
 
 
 def approximate_tangent_line(x, approximate_derivative):
     return (approximate_derivative*x) + b
 
 
-for i in range(5):
+for i in np.arange(0, 10, 2):
     # The point and the "close enough" point
     p2_delta = 0.0001
     x1 = i
@@ -34,13 +38,16 @@ for i in range(5):
     b = y2 - approximate_derivative*x2
     toPlot = [x1-0.9, x1, x1+0.9]
 
-    plt.scatter(x1, y1, c=colors[i])
+    plt.scatter(x1, y1, c=colors[abs(i)])
     plt.plot([point for point in toPlot],
              [approximate_tangent_line(point, approximate_derivative)
               for point in toPlot],
-             c=colors[i])
+             c=colors[abs(i)])
 
     print('Approximate derivative fot f(x)',
           f'where x = {x1} is {approximate_derivative}')
 
 plt.show()
+
+# https://nnfs.io/but
+# https://nnfs.io/ch7
