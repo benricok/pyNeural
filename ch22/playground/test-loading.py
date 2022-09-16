98664a9db652faefe30ff492f08800fac6384c95
Benrico Krog <62467535+benricok@users.noreply.github.com>
2022-09-16 22:01:02 +0200

    update

--- ch22/playground/test-loading.py
+++ ch22/playground/test-loading.py
@@ -1,10 +1,22 @@
 import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
 MODELS = "C:\\Users\\bkadmin\\Documents\\Datasets"
 
 labels = os.listdir(MODELS + '\\fashion_mnist_images\\train')
 print(labels)
 
-files = os.listdir(MODELS + '\\fashion_mnist_images\\train\\0')
+files = os.listdir(MODELS + '\\fashion_mnist_images\\train\\4')
 print(files[:10])
 print(len(files))
+
+image_data = cv2.imread(MODELS + '\\fashion_mnist_images\\train\\4\\0011.png',
+cv2.IMREAD_UNCHANGED)
+
+np.set_printoptions(linewidth=200)
+print(image_data)
+
+plt.imshow(image_data, cmap='gray')
+plt.show()
